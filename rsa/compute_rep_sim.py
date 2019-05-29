943af9a2bf9469b5ac752efa2b15d42ecbaa0ec0
Samira Abnar <samiraabnar@gmail.com>
2019-05-29 23:48:18 +0200

    fix some bugs

--- rsa/compute_rep_sim.py
+++ rsa/compute_rep_sim.py
@@ -19,16 +19,14 @@ import os
 FLAGS = tf.flags.FLAGS
 
 tf.flags.DEFINE_integer('subject_id', 1, 'subject id')
-tf.flags.DEFINE_integer('fold_id', 0, 'fold id')
-tf.flags.DEFINE_list('delays',[-6,-4,-2,0] , 'delay list')
-tf.flags.DEFINE_boolean('cross_delay', False, 'try different train and test delays')
-tf.flags.DEFINE_list('blocks', [1,2,3,4], 'experiment/story blocks to consider')
+tf.flags.DEFINE_list('delay', 0 , 'delay list')
+tf.flags.DEFINE_list('blocks', [1], 'experiment/story blocks to consider')
 
 
 tf.flags.DEFINE_float('alpha', 1, 'alpha')
 tf.flags.DEFINE_string('embedding_dir', 'Data/word_embeddings/glove.6B/glove.6B.300d.txt', 'path to the file containing the embeddings')
 tf.flags.DEFINE_string('brain_data_dir', 'Data/harrypotter/', 'Brain Data Dir')
-tf.flags.DEFINE_string('root', '/Users/iSam/Codes/', 'general path root')
+tf.flags.DEFINE_string('root', '/Users/samiraabnar/Codes/', 'general path root')
 
 tf.flags.DEFINE_enum('text_encoder', 'glove',
                      ['glove','elmo', 'tf_token' ,'universal_large', 'google_lm'], 'which encoder to use')
@@ -66,24 +64,29 @@ def compare_brains_in_regions(brain_regions, regions_to_voxels):
       print(region_name, '\t', np.mean(klz_brain), '\t', np.std(klz_brain))
       plot(klz_brain, labels_brain)
 
-def read_embeddings(a, blocks, selecting_feature, context_mode, selected_steps):
+def read_embeddings(a, blocks, selecting_feature, context_mode, selected_steps, delay):
   time_steps = a['time_steps']
+  print("blocks:", blocks)
+  embedded_steps = []
   for b in blocks:
+    print("read embedding block:", b)
     selected_steps[b][embedding_key + context_mode] = []
     for i in np.arange(len(time_steps[b])):
       current_step = time_steps[b][i]
       if selecting_feature == 'none':
-        if i >= a['start_steps'][b] and i < a['end_steps'][b]:
+        if i >= a['start_steps'][b] and i < a['end_steps'][b] and i+delay > 0 and i+delay < len(a['encoded_stimuli'][b]):
           selected_steps[b][embedding_key + context_mode].append(i)
       elif story_features[selecting_feature][b][current_step] == 1 and i >= a['start_steps'][b] and i < \
-          a['end_steps'][b]:
+          a['end_steps'][b] and i+delay > 0 and i+delay < len(a['encoded_stimuli'][b]):
         selected_steps[b][embedding_key + context_mode].append(i)
 
       if len(np.asarray(a['encoded_stimuli'][b][i]).shape)>1:
         a['encoded_stimuli'][b][i] = np.mean(a['encoded_stimuli'][b][i], axis=0)
+
+    embedded_steps.extend(list(np.asarray(a['encoded_stimuli'][b])[selected_steps[b][embedding_key + context_mode]]))
     print(a['start_steps'][b], a['end_steps'][b])
 
-    return list(np.asarray(a['encoded_stimuli'][b])[selected_steps[b][embedding_key + context_mode]]), selected_steps
+  return embedded_steps, selected_steps
 
 
 
@@ -101,17 +104,16 @@ if __name__ == '__main__':
 
   brain_data_reader = HarryPotterReader(data_dir=hparams.brain_data_dir)
 
-  story_features = np.load('story_features.npy').item()
-  delay = 10
-  blocks = [1,2,3,4]
+  story_features = np.load('../story_features.npy').item()
+  delay = -1
   selected_steps = {}
   print(story_features.keys())
-  selecting_feature = 'quote' # none | end | ch | quote
+  selecting_feature = 'none' # none | end | ch | quote
 
   voxel_to_regions = {}
   regions_to_voxels = {}
   brain_fs = {}
-  for subject in [1,2,3,4,5,7]:
+  for subject in np.arange(1,9):
     voxel_to_regions[subject], regions_to_voxels[subject] = brain_data_reader.get_voxel_to_region_mapping(subject_id=subject)
     brain_fs[subject] = VarianceFeatureSelection()
 
@@ -137,13 +139,13 @@ if __name__ == '__main__':
   embeddings = {}
   labels = {}
 
-  encoder_types = ['elmo','universal_large', 'glove','bert'] #, 'google_lm','tf_token']
+  encoder_types = ['google_lm','elmo','universal_large', 'glove','bert'] #, 'google_lm','tf_token']
   embedding_types = {
     'universal_large': ['none'],
     'google_lm': ['lstm_0', 'lstm_1'],
     'glove': ['none'],
     'elmo': ['lstm_outputs1','lstm_outputs2'],
-    'bert': ['0','6','11'],
+    'bert': ['0', '6','11'],
     'tf_token': ['none']
   }
 
@@ -168,8 +170,9 @@ if __name__ == '__main__':
                             encoder_type + '_' + embedding_type + '_none_' + str(0) + '-0_onlypast-True'),
                'rb'))
 
-      embedded_steps, selected_steps = read_embeddings(a,blocks, selecting_feature,
-                                                       context_mode="_none", selected_steps=selected_steps)
+      embedded_steps, selected_steps = read_embeddings(a,FLAGS.blocks, selecting_feature,
+                                                       context_mode="_none", selected_steps=selected_steps, delay=delay)
+
       embeddings[embedding_key][-1].extend(embedded_steps)
       embeddings[embedding_key][-1] = np.asarray(embeddings[embedding_key][-1])
       labels[embedding_key].append(embedding_key + '_context_none')
@@ -189,26 +192,20 @@ if __name__ == '__main__':
             open(os.path.join(hparams.emb_save_dir,
                               encoder_type+'_'+embedding_type+'_sentence_' + str(context_size) + '-0_onlypast-True'),
                  'rb'))
-        embedded_steps, selected_steps = read_embeddings(a, blocks, selecting_feature,
+
+        embedded_steps, selected_steps = read_embeddings(a, FLAGS.blocks, selecting_feature,
                                                          context_mode="_"+str(context_size),
-                                                         selected_steps=selected_steps)
+                                                         selected_steps=selected_steps, delay=delay)
+
+        print("shape:" ,np.asarray(embedded_steps).shape)
         embeddings[embedding_key][-1].extend(embedded_steps)
         embeddings[embedding_key][-1] = np.asarray(embeddings[embedding_key][-1])
         labels[embedding_key].append(embedding_key+'_context_' + str(context_size))
 
-  print("selected steps (they should be the same:")
-  brain_selected_steps = {1:[],2:[],3:[],4:[]}
-  for b in [1,2,3,4]:
+  brain_selected_steps = {1: [], 2: [], 3: [], 4: []}
+  for b in [1, 2, 3, 4]:
     for sel in selected_steps[b]:
-      brain_selected_steps[b] = list(np.asarray(selected_steps[b][sel])+delay)
-
-  all_embeddings = []
-  all_labels = []
-  for key in embeddings.keys():
-    all_embeddings += embeddings[key]
-    all_labels += labels[key]
-
-
+      brain_selected_steps[b] = list(np.asarray(selected_steps[b][sel]) + delay)
 
   #Load Brains
   brains = []
@@ -217,9 +214,9 @@ if __name__ == '__main__':
   brain_regions_labels = []
   for i in np.arange(1, 9):
     brains.append([])
-    a = pickle.load(open(os.path.join(hparams.emb_save_dir,str(i) + '_Brain'),
+    a = pickle.load(open(os.path.join(hparams.emb_save_dir, str(i) + '_Brain'),
                          'rb'))
-    for b in blocks:
+    for b in FLAGS.blocks:
       brains[-1].extend(a['brain_activations'][b][brain_selected_steps[b]])
 
     brains[-1] = np.asarray(brains[-1])
@@ -237,6 +234,13 @@ if __name__ == '__main__':
     brains[-1] = brains[-1][:, selected_voxels]
     brain_labels.append('brain_' + str(i))
 
+  all_embeddings = []
+  all_labels = []
+  for key in embeddings.keys():
+    all_embeddings += embeddings[key]
+    all_labels += labels[key]
+
+
   uniform_random_300 = np.random.uniform(low=-1, high=1, size=(all_embeddings[0].shape[0],300))
   normal_random_300 = np.random.standard_normal(size=(all_embeddings[0].shape[0],300))
   uniform_random_100 = np.random.uniform(low=-1, high=1, size=(all_embeddings[0].shape[0], 100))
@@ -252,6 +256,8 @@ if __name__ == '__main__':
   random_labels = ['uniform_random_300', 'normal_ransom_300',
                    'uniform_random_100', 'normal_random_100',
                    'uniform_random_1000', 'normal_random_1000']
+
+  print("brain shapes:", brain_regions[0].shape)
   x, C = get_dists(brain_regions + all_embeddings + randoms)
   klz, prz, labels_, p_vals = compute_dist_of_dists(x, C, brain_regions_labels + all_labels + random_labels)
   plot(prz, labels_)
@@ -278,4 +284,4 @@ if __name__ == '__main__':
               "prz": prz,
               "labels_": labels_,
               "p_vals": p_vals}
-  np.save("rsa_results_all_full_brain_"+str(delay)+"_"+selecting_feature, dic2save)
+  np.save('_'.join(list(map(str,FLAGS.blocks)))+"rsa_results_all_full_brain_"+str(delay)+"_"+selecting_feature, dic2save)
